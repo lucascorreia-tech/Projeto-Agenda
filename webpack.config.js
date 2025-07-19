f0fbe733de60ed01808fd4afb5be799ea3412003
LucCoreDev <lucasrcorreia23@gmail.com>
2025-07-19 13:17:32 -0300

    "chore: estrutura inicial do projeto"

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  mode: 'development',
+
+  entry: './frontend/main.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public','assets', 'js'),
+    clean: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js']
+  },
+
+  watchOptions: {
+    ignored: /node_modules/
+  },
+
+  devtool: 'source-map'
+};
