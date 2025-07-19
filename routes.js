8245182ebaa8e657662db7b220c35b091d12db2f
LucCoreDev <lucasrcorreia23@gmail.com>
2025-07-19 19:08:39 -0300

    feat: Cria a rota para login ou cadastro

--- routes.js
+++ routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import homeController from './src/controllers/homeController.js';
+import loginController from './src/controllers/loginController.js';
 const routes = express.Router();
 
 // Rotas da home
-routes.get('/', homeController.paginaInicial);
+routes.get('/', homeController.index);
+
+// Rotas de Login
+routes.get('/login/index', loginController.index);
 
 
 export default routes;
