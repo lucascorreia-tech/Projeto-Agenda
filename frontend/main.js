abf37c5a2998cca118fa5c5af6953ee2b91e6bb7
LucCoreDev <lucasrcorreia23@gmail.com>
2025-07-23 15:51:24 -0300

    feat: Valida no lado do Front-end os formulários de cadastro e login

--- frontend/main.js
+++ frontend/main.js
@@ -2,3 +2,10 @@ import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './modules/Login.js';
+
+// Valida o formulário do login e cadastro
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+login.init();
+cadastro.init();
